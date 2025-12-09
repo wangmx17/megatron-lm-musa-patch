2bda8d14efe66cebcaa4d13229db4effe3cba0a8
CuiXiaoteng <xiaoteng.cui@mthreads.com>
2025-12-09 14:53:14 +0800

    add 2host script and update WORLD_SIZE

---
+++ examples/kimi-k2/kimi-k2/pretrain_kimi_2host.sh
@@ -0,0 +1,247 @@
+#!/bin/bash
+
+# Runs the "175B" parameter model
+
+# Please change the following envrioment variables
+# base on the cluster configuration
+set -u
+  WORK_HOME=$1
+  PATCH_HOME=$2
+  EXPNAME=$3
+  HOSTFILE=$4
+  DATA_DIR=$5
+  TP_SIZE=$6
+  PP_SIZE=$7
+  EP_SIZE=$8
+  MICRO_BATCH_SIZE=$9
+  GLOBAL_BATCH_SIZE=${10}
+  TOKENIZED_MODEL=${11}
+  RDZV_ID=${12}
+set +u
+export ENABLE_PROFILER=0
+export PROFILER_FREQ=4
+# export MUSA_LAUNCH_BLOCKING=1
+export OMP_NUM_THREADS=4
+export MUSA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7'
+export MUSA_KERNEL_TIMEOUT=3200000
+export ACCELERATOR_BACKEND="musa"
+export MCCL_PROTOS=2
+export MCCL_CHECK_POINTERS=0
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export MCCL_IB_GID_INDEX=3
+export MUSA_BLOCK_SCHEDULE_MODE=1
+export MCCL_ALGOS=1
+export MCCL_BUFFSIZE=20480000
+
+export USE_RECOMPUTE_VARIANCE=0
+export ENABLE_D2H_IN_PERMUTATION=0
+export NO_LOSS_REDUCE=0
+export USE_MUSA_MOE=1
+
+MEGATRON_PATH=${PATCH_HOME}/../Megatron-LM
+export PYTHONPATH=${MEGATRON_PATH}:${PATCH_HOME}:$PYTHONPATH
+
+if [ ! -d "${MEGATRON_PATH}/build" ]; then
+    cd "${MEGATRON_PATH}"
+    python setup.py build_ext --inplace
+    cd -
+fi
+
+CHECKPOINT_PATH=$WORK_HOME/checkpoints/$EXPNAME
+mkdir -p $CHECKPOINT_PATH
+DATA_PATH=$DATA_DIR
+
+
+LOG_PATH=$WORK_HOME/logs/$RDZV_ID
+mkdir -p $LOG_PATH
+cp $0 $LOG_PATH/
+TB_PATH=$WORK_HOME/tboard/$EXPNAME
+mkdir -p $TB_PATH
+WB_PATH=$WORK_HOME/wandb/$EXPNAME
+mkdir -p $WB_PATH
+
+export DUMP_MEMORY_SNAPSHOT=0
+export MEMORY_SNAPSHOT_PATH=$WORK_HOME/mem_snapshot/$RDZV_ID
+mkdir -p $MEMORY_SNAPSHOT_PATH
+export RDZV_ID=$RDZV_ID
+
+#VMM
+export PYTORCH_MUSA_ALLOC_CONF="expandable_segments:True"
+export TORCH_MCCL_AVOID_RECORD_STREAMS=1 
+
+export NODE_ADDR=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2;}'|tr -d "addr:"|head -n 1) # tail for cuda/ head for musa
+export GPUS_PER_NODE=8
+export NUM_NODES=$(cat $HOSTFILE | wc -l)
+export MASTER_ADDR=$(head -n1 $HOSTFILE | awk '{print $1;}')
+export NODE_RANK=$(awk -v node_addr="$NODE_ADDR" '{ranks[$1]=(FNR-1);} END {print ranks[node_addr];}' $HOSTFILE)
+export MASTER_PORT=12356
+# export MUSA_LAUNCH_BLOCKING=1
+# export MCCL_DEBUG=INFO
+
+DISTRIBUTED_ARGS=(
+    --nproc_per_node $GPUS_PER_NODE 
+    --nnodes $NUM_NODES 
+    --node_rank $NODE_RANK 
+    --master_addr $MASTER_ADDR 
+    --master_port $MASTER_PORT
+    --log_dir $WORK_HOME/output_log/$RDZV_ID/$EXPNAME/$NODE_RANK
+    --redirects 3
+)
+
+MODEL_ARGS=(
+    --num-layers 2  # 61 
+    --hidden-size 7168
+    --num-attention-heads 64
+    --seq-length 4096 
+    --max-position-embeddings 4096 
+    --norm-epsilon 1e-6 
+    --attention-dropout 0.0 
+    --hidden-dropout 0.0 
+    --disable-bias-linear 
+    --vocab-size 163840
+    --ffn-hidden-size 18432
+    --position-embedding-type rope 
+    --no-position-embedding 
+    --swiglu 
+    --normalization RMSNorm
+    --untie-embeddings-and-output-weights
+    --rope-type yarn
+)
+
+# 24414062 1T
+TRAINING_ARGS=(
+    --seed 42 
+    --micro-batch-size $MICRO_BATCH_SIZE 
+    --global-batch-size $GLOBAL_BATCH_SIZE  
+    --train-samples 24414062 
+    --init-method-std  0.006 # 0.02 in HF config, but 0.006 in the paper 
+    --use-mcore-models 
+    # --no-gradient-accumulation-fusion 
+    --no-bias-dropout-fusion
+    # --no-bias-swiglu-fusion
+    --use-distributed-optimizer 
+    --use-flash-attn 
+    --sequence-parallel 
+    # --recompute-granularity full 
+    # --recompute-method block 
+    # --recompute-num-layers 1
+    --distributed-backend nccl
+    --multi-latent-attention
+    --qk-layernorm
+    --enable-experimental
+
+    --recompute-granularity selective
+    --recompute-modules mla_up_proj layernorm moe
+)
+
+MLA_ARGS=(
+    --q-lora-rank 1536
+    --kv-lora-rank 512
+    --qk-head-dim 128
+    --qk-pos-emb-head-dim 64
+    --v-head-dim 128
+    --rotary-scaling-factor 1
+)
+REGULARIZATION_ARGS=(
+    --weight-decay 0.1 
+    --adam-beta1 0.9 
+    --adam-beta2 0.95 
+    --clip-grad 1.0 
+)
+
+WARMUP_STEPS=2000
+WARMUP_SAMPLES=$((WARMUP_STEPS * GLOBAL_BATCH_SIZE))
+
+LEARNING_RATE_ARGS=(
+    --lr 2.2e-4 
+    --lr-decay-style cosine 
+    --lr-warmup-samples ${WARMUP_SAMPLES} 
+    --min-lr 1.5e-6 
+    --initial-loss-scale 65536 
+    --min-loss-scale 1.0 
+)
+
+MODEL_PARALLEL_ARGS=(
+	--tensor-model-parallel-size $TP_SIZE  
+	--pipeline-model-parallel-size $PP_SIZE 
+    # --decoder-last-pipeline-num-layers 1
+)
+
+MIXED_PRECISION_ARGS=(
+    --bf16 
+    --attention-softmax-in-fp32 
+    --no-masked-softmax-fusion 
+    --accumulate-allreduce-grads-in-fp32
+)
+
+DATA_ARGS=(
+    --data-path $DATA_PATH
+    --tokenizer-type NullTokenizer
+    # --tokenizer-model ${TOKENIZED_MODEL}
+    --split 1
+)
+
+EVAL_AND_LOGGING_ARGS=(
+    --log-interval 1
+    --log-throughput
+    --save-interval 100000 
+    --eval-interval 1 
+    --save $CHECKPOINT_PATH 
+    --load $CHECKPOINT_PATH 
+    --eval-iters 0
+    --tensorboard-dir $TB_PATH 
+)
+
+NUM_LAYERS=$(echo "${MODEL_ARGS[@]}" | grep -oP '(?<=--num-layers )\d+')
+NUM_LAYERS_MINUS_ONE=$((NUM_LAYERS - 1))
+MOE_LAYER_FREQ="([0]*1+[1]*${NUM_LAYERS_MINUS_ONE})*1"
+MOE_ARGS=(
+    --num-experts 384
+    --expert-model-parallel-size $EP_SIZE
+    --moe-token-dispatcher-type alltoall
+    # --moe-router-num-groups 8
+    # --moe-router-group-topk 4
+    --moe-router-topk 8
+    --moe-router-score-function sigmoid
+    --moe-router-topk-scaling-factor 2.5
+    --moe-ffn-hidden-size 2048
+    --moe-shared-expert-intermediate-size 2048
+    --moe-layer-freq "$MOE_LAYER_FREQ"
+    --moe-grouped-gemm
+    --moe-router-enable-expert-bias
+    --moe-router-bias-update-rate 1e-3
+    --moe-router-dtype fp32
+    # --overlap-moe-expert-parallel-comm
+    # --moe-permute-fusion
+)
+
+TRANSFORMER_ENGINE_ARGS=(
+    --transformer-impl transformer_engine
+    --fp8-format e4m3
+    --fp8-param-gather
+    --fp8-recipe mxfp8
+)
+    
+MULTI_TOKEN_PREDICTION_ARGS=(
+    --use-multi-token-prediction
+    --mtp-coeff 1e-4
+    --mtp-depth 1
+)
+
+cmd="torchrun ${DISTRIBUTED_ARGS[@]} $WORK_HOME/pretrain_kimi.py \
+        ${MODEL_ARGS[@]} \
+        ${TRAINING_ARGS[@]} \
+        ${REGULARIZATION_ARGS[@]}
+        ${LEARNING_RATE_ARGS[@]} \
+        ${MODEL_PARALLEL_ARGS[@]} \
+        ${MIXED_PRECISION_ARGS[@]}
+        ${DATA_ARGS[@]} \
+        ${MOE_ARGS[@]} \
+        ${MLA_ARGS[@]} \
+        ${EVAL_AND_LOGGING_ARGS[@]} \
+        ${TRANSFORMER_ENGINE_ARGS[@]} \
+    "
+echo $cmd
+$cmd
+
+# msys profile -t musa --gpu-metrics-set=1 -o S5000-dsv3-cpu-offload-buffer --device 0 
